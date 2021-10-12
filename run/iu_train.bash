73ec3b88901d6f02d8edb9ae48b2828f9acf2721
Vidura Prasangana <vidura.prasangana16@gmail.com>
2021-10-12 19:50:53 +0530

    run script

--- run/iu_train.bash
+++ run/iu_train.bash
@@ -10,7 +10,7 @@ cp $0 $output/run.bash
 # See Readme.md for option details.
 CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/captioning.py \
-    --train train --valid "" \
+    --train train --valid test \
     --llayers 9 --xlayers 5 --rlayers 5 \
     --batchSize 32 --optim bert --lr 5e-5 --epochs 4 \
     --tqdm --output $output ${@:3}
